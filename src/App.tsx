76e8cdcb8c36007e41ab9e47d0ae259c7f5b53f2
Alex <a>
2020-10-10 17:28:54 +0300

    feat: added dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Quotes from "./components/Quotes/Quotes";
 import {Header} from "./components/Header";
+import {Button, Dropdown} from "react-bootstrap";
+import SwitchThemeButton from "./components/SwitchThemeButton";
 
 function App() {
+    const [darkTheme, setDarkTheme] = useState(false);
+    let cl = darkTheme ? "main_window"+" bg-dark" : "main_window";
     return (
-        <div>
-            <Header/>
-            <Quotes/>
+        <div className={cl}>
+            <Header darkTheme={darkTheme}/>
+            <Quotes darkTheme={darkTheme}/>
+
+            <SwitchThemeButton darkTheme={darkTheme} callBack={() => setDarkTheme(!darkTheme)}/>
         </div>
     );
 
