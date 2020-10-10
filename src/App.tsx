43dfd2c0696165ecc759b7225df54bb7c4e6af92
Alex <a>
2020-10-10 18:42:07 +0300

    fix: fixed dark on mobile
    feat: added localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Quotes from "./components/Quotes/Quotes";
 import {Header} from "./components/Header";
@@ -7,6 +7,25 @@ import SwitchThemeButton from "./components/SwitchThemeButton";
 
 function App() {
     const [darkTheme, setDarkTheme] = useState(false);
+    useEffect(() => {
+        if (!localStorage.getItem('theme')) {
+            localStorage.setItem('theme', 'light');
+        }
+        if (localStorage.getItem('theme') === "dark") {
+            setDarkTheme(true);
+        }
+    }, []);
+
+
+    const changeTheme = () => {
+
+        if (darkTheme) {
+            localStorage.setItem('theme', 'light');
+        } else {
+            localStorage.setItem('theme', 'dark');
+        }
+        setDarkTheme(!darkTheme);
+    };
     let cl = darkTheme ? "main_window" + " bg-dark" : "main_window";
     let bd = document.body;
     bd.className = darkTheme ? "bg-dark" : "";
@@ -15,7 +34,7 @@ function App() {
             <Header darkTheme={darkTheme}/>
             <Quotes darkTheme={darkTheme}/>
 
-            <SwitchThemeButton darkTheme={darkTheme} callBack={() => setDarkTheme(!darkTheme)}/>
+            <SwitchThemeButton darkTheme={darkTheme} callBack={changeTheme}/>
         </div>
     );
 
